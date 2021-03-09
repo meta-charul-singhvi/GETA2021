f4642fcb75295b740face6a7a85d1bd837165a63
meta-charul-singhvi <79201914+meta-charul-singhvi@users.noreply.github.com>
2021-03-09 08:33:57 +0530

    Question2

---
+++ QueueMain.java
@@ -0,0 +1,53 @@
+/*
+ * @author : Charul Singhvi
+ * @date : 08-03-2021
+ */
+package Stack_Queues;
+
+import java.util.Scanner;
+
+public class QueueMain {
+
+	public static void main(String[] args) {
+		QueueMain mainObject = new QueueMain();
+		mainObject.userInput ();	
+	}
+	
+	private void userInput(){
+		System.out.println("Enter size of queue : ");
+		Scanner scan = new Scanner(System.in);
+		try {
+			
+			int size = scan.nextInt();
+			while(size<=0){
+				System.out.println("Enter valid size of queue : ");
+				size = scan.nextInt();
+			}
+			QueueInterface queueObject = new Queue(size);
+		
+			while (true){
+				System.out.println("Enter 1 to enqueue \n	2 to dequeue\n	3 to display queue");
+				int choice = scan.nextInt();
+				if (choice==1){
+					System.out.println("Enter Element to be pushed");
+					int element = scan.nextInt();
+					queueObject.enQueue(element);
+				}
+				else if (choice==2){
+					queueObject.deQueue();
+				}
+				else if (choice==3){
+					break;
+				}
+				else{
+					throw new Exception ("Invalid choice");
+				}
+			}
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
