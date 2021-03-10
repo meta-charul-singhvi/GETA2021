9f38236b7918e862a89b473952876a128bbcf6e9
meta-charul-singhvi <79201914+meta-charul-singhvi@users.noreply.github.com>
2021-03-10 22:56:07 +0530

    Question1 1.1

--- StringOperations.java
+++ StringOperations.java
@@ -1,10 +1,10 @@
 /* @author Charul Singhvi
- *@date 03-03-2021
- *The class StringOpeations is used to perfrom basic operations on strings such as:
- *	@stringCompare returns the output as 1 if the 2 strings are equal else 0.
- *	@reverseString returns the reversed string.
- *	@reverseCaseOfString returns the string with reversed cases such as lower to upper and vica versa.
- *	@largestWordInString returns the largest word available in the string.
+ * @date 03-03-2021
+ * The class StringOpeations is used to perfrom basic operations on strings such as:
+ * @stringCompare returns the output as 1 if the 2 strings are equal else 0.
+ * @reverseString returns the reversed string.
+ * @reverseCaseOfString returns the string with reversed cases such as lower to upper and vica versa.
+ * @largestWordInString returns the largest word available in the string.
 */
 
 package ss;
@@ -40,20 +40,25 @@ public class StringOperations {
 		return revStr;
 	}
 	
+	
+	
 	/*
 	 * Checking the case of particular character and reversing it.
 	 */
-	public static String reverseCaseOfString(String str, int len){
+	public static String reverseCaseOfString(String str, int len)throws ArrayIndexOutOfBoundsException{
 		String revCaseStr = "";
-		for(int i=0; i<len; i++){
-			//reversing the case
-			if (str.charAt(i)==' ')
-				revCaseStr += ' ';
-			else if(Character.isLowerCase(str.charAt(i)))
-				revCaseStr += Character.toUpperCase(str.charAt(i));
-			else if(Character.isUpperCase(str.charAt(i)))
-				revCaseStr += Character.toLowerCase(str.charAt(i));
+		String[] revCaseArray = str.split(" ");
+		
+		for(String s : revCaseArray){
+			for(int i=0 ; i<s.length(); i++){
+				if((s.charAt(i))>='A' && (s.charAt(i))<='Z')
+					revCaseStr += (char)((int)s.charAt(i)+32);
+				else if((s.charAt(i))>='a' && (s.charAt(i))<='z')
+					revCaseStr += (char)((int)s.charAt(i)-32);
+			}
+			revCaseStr += ' ';
 		}
+		
 		return revCaseStr;
 	}
 	
@@ -73,51 +78,60 @@ public class StringOperations {
 		}
 	
 	
-	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws ArrayIndexOutOfBoundsException{
+		try{
 		
-		int len1,len2;
-		String s1 ,s2;
-		 
-		System.out.println("Enter the 1st string");
-		s1=scan.nextLine();
-		System.out.println("Enter the 2nd string");
-		s2=scan.nextLine();
-		 
-		len1=s1.length();
-		len2=s2.length();
-		 
-		/*
-		 * Calling function to compare 2 Strings
-		 */
-		 int compareString = stringCompare(s1, s2, len1, len2);
-		 if(compareString==1)
-			 System.out.println("The two string are EQUAL!!!\n");
-		 else
-			 System.out.println("The two string are NOT EQUAL!!!\n");
-		 
-		 
-		 /*
-		  * Calling function to reverse a String
-		  */
-		 String revStr = reverseString(s1, len1);
-		 System.out.println("The reversed String1 is : " +revStr+"\n");
-		 
-		 
-		 /*
-		  * Calling function to reverse the case of String
-		  */
-		 String revCaseString = reverseCaseOfString(s1, len1);
-		 System.out.println("The reversed Case String1 is : " +revCaseString+"\n");
-		 
-		 
-		 /*
-		  * Calling function to find Largest word in String
-		  */
-		 String largestWordInString = largestWordInString(s1, len1);
-		 System.out.println("The largest word in the String1 is : " +largestWordInString+"\n");
-		 
-		 scan.close();
+			Scanner scan = new Scanner(System.in);
+			
+			int len1,len2;
+			String s1 ,s2;
+			 
+			System.out.print("Enter the 1st string : ");
+			s1=scan.nextLine();
+			System.out.print("Enter the 2nd string : ");
+			s2=scan.nextLine();
+			 
+			len1=s1.length();
+			len2=s2.length();
+			 
+			/*
+			 * Calling function to compare 2 Strings
+			 */
+			 int compareString = stringCompare(s1, s2, len1, len2);
+			 System.out.print("The two strings '"+s1+"' and '"+s2);
+			 if(compareString==1)
+				 System.out.println("' are EQUAL!!!\n");
+			 else
+				 System.out.println("' are NOT EQUAL!!!\n");
+			 
+			 
+			 /*
+			  * Calling function to reverse a String
+			  */
+			 String revStr = reverseString(s1, len1);
+			 System.out.println("The reversed String1 is : " +revStr+"\n");
+			 
+			 
+			 /*
+			  * Calling function to reverse the case of String
+			  */
+			 String revCaseString = reverseCaseOfString(s1, len1);
+			 System.out.println("The reversed Case String1 is : " +revCaseString+"\n");
+			 
+			 
+			 /*
+			  * Calling function to find Largest word in String
+			  */
+			 String largestWordInString = largestWordInString(s1, len1);
+			 System.out.println("The largest word in the String1 is : " +largestWordInString+"\n");
+			 
+			 scan.close();
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			System.out.print("This isn't a valid string!");
+		}
 		
 	}
 }
+//consider spaces
+
